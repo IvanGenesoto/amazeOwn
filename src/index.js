9dc15fbe73e7c18894eb8fc45c8a8e1a92c2a154
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2017-07-26 22:04:15 -0700

    Removed unneeded code.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const bodyParser = require('body-parser')
 const data = require('./data')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.json())
 
 app.get('/featured', (req, res) => {
   res.json(data)
