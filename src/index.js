c52ce3d2aada38cbe1f30ec9cba0f6526fb370d7
IvanGenesoto <igenesoto@gmail.com>
2017-07-26 21:02:45 -0700

    Made item details and cart items load from server. #20

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ app.get('/featured', (req, res) => {
   res.json(data)
 })
 
+app.get('/items/:id', (req, res) => {
+  const match = data.find(item => item.id === +req.params.id)
+  res.json(match)
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
