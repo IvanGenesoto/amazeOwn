3a67439f23e8d676d0458eb754f05c2e0ff3a7ce
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2017-07-26 17:09:13 -0700

    Made front-page items load from server. #20

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
+const data = require('./data')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+
+app.get('/featured', (req, res) => {
+  res.json(data)
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
