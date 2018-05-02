45922784f81870546416d7f21cfcb798e9ce3f08
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2018-05-02 00:47:29 -0700

    Removed border from thumbnails. #29

--- src/client/state/methods/preload-logo-frames.js
+++ src/client/state/methods/preload-logo-frames.js
@@ -2,12 +2,12 @@ module.exports = function preloadLogoFrames(frame) {
   const {c, logoFrames} = this
   if (frame < 6) {
     frame += 1
-    const logoFrameId = 'logo-frame-' + frame
-    const logoFrame = 'images/logo-frame/' + frame + '.png'
+    const src = 'images/logo-frame/' + frame + '.png'
+    const id = 'frame-' + frame
     const $logoFrame = c('img', {
+      src,
+      id,
       'class': 'logo-frame',
-      'id': logoFrameId,
-      'src': logoFrame,
       'data-target': '.bs-example-modal-sm',
       'data-toggle': 'modal'
     })
