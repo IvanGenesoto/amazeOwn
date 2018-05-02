6ffbb15ac7cd9e918b3db52bd8b8564efd95263c
Ivan Genesoto <igenesoto@gmail.com>
2018-05-02 00:47:29 -0700

    Removed border from thumbnails. #29

--- src/client/state/methods/render/details-view.js
+++ src/client/state/methods/render/details-view.js
@@ -2,43 +2,44 @@ module.exports = function renderDetailsView(item) {
   const {c, getStars, $detailsView, customizeButton} = this
   const price = item.price.toFixed(2)
   const stars = getStars(item.rating)
-  const $row = c('div', {'class': 'row'}, [
-    c('div', {'class': 'col-xs-1'}, [
-      c('div', {'class': 'row'}, [
-        c('div', {'class': 'col-xs-12'}, [
-          c('img', {'class': 'thumbnail image', 'src': item.image2})
-        ])
-      ]),
-      c('div', {'class': 'row'}, [
-        c('div', {'class': 'col-xs-12'}, [
-          c('img', {'class': 'thumbnail image', 'src': item.image3})
+  const $row =
+    c('div', {'class': 'row'}, [
+      c('div', {'class': 'col-xs-1'}, [
+        c('div', {'class': 'row'}, [
+          c('div', {'class': 'col-xs-12'}, [
+            c('img', {'class': 'thumbnailed image', 'src': item.image2})
+          ])
         ]),
         c('div', {'class': 'row'}, [
           c('div', {'class': 'col-xs-12'}, [
-            c('img', {'class': 'thumbnail image', 'src': item.image4})
+            c('img', {'class': 'thumbnailed image', 'src': item.image3})
+          ])
+        ]),
+        c('div', {'class': 'row'}, [
+          c('div', {'class': 'col-xs-12'}, [
+            c('img', {'class': 'thumbnailed image', 'src': item.image4})
           ])
         ])
-      ])
-    ]),
-    c('div', {'class': 'col-xs-4'}, [
-      c('img', {'class': 'details image', 'src': item.image})
-    ]),
-    c('div', {'class': 'col-xs-7 details'}, [
-      c('h2', null, item.name),
-      c('img', {'src': stars, 'class': 'rating'}),
-      c('div', {'class': 'row'}, [
-        c('div', {'class': 'col-xs-6 price-column'}, [
-          c('h3', {'class': 'dollar'}, '$'),
-          c('h3', {'class': 'price'}, price)
-        ]),
-        c('div', {'class': 'col-xs-6'}, [
-          c('button', {'class': 'btn btn-default button', 'id': 'add-cart', 'data-id': item.id}, 'ADD TO CART')
-        ])
       ]),
-      c('hr'),
-      c('p', null, item.description)
+      c('div', {'class': 'col-xs-4'}, [
+        c('img', {'class': 'details image', 'src': item.image})
+      ]),
+      c('div', {'class': 'col-xs-7 details'}, [
+        c('h2', null, item.name),
+        c('img', {'src': stars, 'class': 'rating'}),
+        c('div', {'class': 'row'}, [
+          c('div', {'class': 'col-xs-6 price-column'}, [
+            c('h3', {'class': 'dollar'}, '$'),
+            c('h3', {'class': 'price'}, price)
+          ]),
+          c('div', {'class': 'col-xs-6'}, [
+            c('button', {'class': 'btn btn-default button', 'id': 'add-cart', 'data-id': item.id}, 'ADD TO CART')
+          ])
+        ]),
+        c('hr'),
+        c('p', null, item.description)
+      ])
     ])
-  ])
   $detailsView.appendChild($row)
   customizeButton('#add-cart')
 }
