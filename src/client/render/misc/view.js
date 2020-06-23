1160cc958fb907ab0b1db0f54fa77fd8ea6fadfa
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2020-06-22 23:56:17 -0700

    User's history persists. #36

--- src/client/render/misc/view.js
+++ src/client/render/misc/view.js
@@ -1,20 +1,39 @@
-module.exports = function renderView(name, ...rest) {
+module.exports = function renderView(name, historyKit, argument) {
 
   const state = this
+  const {shouldAlterHistory} = state
+  const {pathName, hash = '', parameter = '', shouldReplaceState} = historyKit || {}
+  const {origin} = location
+  const isArray = Array.isArray(parameter)
+  const parameter_ = parameter && !isArray ? '/' + parameter : ''
+  const {title} = document
+  const title_ = title + ' ' + name
+  const replacingNames = ['confirm-order', 'confirmation']
+  const shouldReplace_ = shouldReplaceState || replacingNames.includes(name)
+  const historyMethod = shouldReplace_ ? 'replaceState' : 'pushState'
+  const shouldAlterHistory_ = shouldAlterHistory || shouldReplace_
+  const historyState = historyKit || name
+  const $app = document.getElementById('app')
+
+  const url =
+      hash || parameter_ ? hash + parameter_
+    : pathName === 'featured' ? origin
+    : '#' + name
 
   const renderViewByName = {
     cart: state.renderCartView,
     checkout: state.renderCheckoutView,
-    confirmOrder: state.renderConfirmOrderView,
+    'confirm-order': state.renderConfirmOrderView,
     confirmation: state.renderConfirmationView,
     item: state.renderItemView,
     list: state.renderListView
   }
 
   const render = renderViewByName[name]
-  const $app = document.getElementById('app')
 
+  shouldAlterHistory_ && history[historyMethod](historyState, title_, url)
+  state.shouldAlterHistory = true
   $app.innerHTML = ''
   state.renderNav()
-  render.call(state, ...rest)
+  render.call(state, argument)
 }
